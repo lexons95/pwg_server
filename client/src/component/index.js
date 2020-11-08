0d2ddfd75febc1cf9500ba66ea7e8219581eabf8
shj <lexons95@hotmail.com>
2020-11-08 21:33:15 +0800

    update
    remove client

--- client/src/component/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Layout01 } from './layout/Layout01.js';
-
-export { default as Header01 } from './header/Header01.js';
-export { default as Header02 } from './header/Header02.js';
--- client/src/component/page/Profile.js
+++
@@ -1,7 +0,0 @@
-import React, {useState} from 'react';
-
-const Profile = (props) => {
-  return null;
-}
-
-export default Profile;
